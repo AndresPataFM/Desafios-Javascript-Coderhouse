047eb6a0ae7dbae9f7865dc669347a22dba3e1fe
Andrés Pata FM <apfm21@Gmail.com>
2021-09-13 23:30:47 -0300

    test

---
+++ Desafios/Desafío1.js
@@ -0,0 +1,15 @@
+let valor1 = Number(prompt('Ingresa el primer número'));
+let valor2 = Number(prompt('Ingresa el segundo número'));
+
+let sumaValores = valor1+valor2;
+let restaValores = valor1-valor2;
+let multiplicarValores = valor1*valor2;
+let dividirValores = valor1+valor2;
+
+
+alert(
+    `Al sumar los valores se obtiene ${sumaValores}.
+    Al restar los valores se obtiene ${restaValores}.
+    Al multiplicar los valores se obtiene ${multiplicarValores}.
+    Al dividir klos valores se obtiene ${dividirValores}`
+)
